13fac606a887bdf29203004569344a05c659ff54
julib111 <150290292+julib111@users.noreply.github.com>
2025-11-18 16:36:52 -0500

    feat: creacion de interfaces

--- src/main/java/co/edu/uniquindio/gestiondesastres/App.java
+++ src/main/java/co/edu/uniquindio/gestiondesastres/App.java
@@ -1,28 +1,14 @@
 package co.edu.uniquindio.gestiondesastres;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import org.jgrapht.Graph;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
-public class App extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/gestiondesastres/view/MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
-        stage.setTitle("Sistema de Gestión de Desastres Naturales");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-
+public class App 
+{
+    public static void main( String[] args )
+    {
         Graph<String, DefaultWeightedEdge> grafo = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
 
         grafo.addVertex("Bogota");
